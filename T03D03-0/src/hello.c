9f6fa523eb0b3446409bc68dc108b6235834beae
Your Name <you@example.com>
2023-01-15 23:34:04 +0300

    s

---
+++ T03D03-0/src/hello.c
@@ -0,0 +1,10 @@
+//  d = int
+//  float/double = f    /// 4.1234
+//  string (char *) = s // " stroka "
+//  char = c // ''
+#include <stdio.h>
+
+int main() {
+    printf("Hello, AI!");
+    return 0;
+}
